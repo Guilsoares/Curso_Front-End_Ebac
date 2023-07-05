7fb79ac18cbefe6118cb23cd6ab06e8044f1b062
Guilherme Soares <g.l.soares@outlook.com>
2023-07-05 11:08:21 -0300

    .

--- Modulo 06/Exercicio_mod06/ambiente.js
+++
@@ -1,8 +0,0 @@
-function calculaArea(a, b){
-    let soma = a * b
-    console.log(`A altura informada foi ${a}.`)
-    console.log(`A base informada foi ${b}.`)
-    console.log(`A multiplicação entre ${a} e ${b} é de ${soma} !!!`)
-}
-
-calculaArea(8, 5)
