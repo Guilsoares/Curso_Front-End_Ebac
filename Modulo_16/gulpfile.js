2d77fe86727e4a3a2e8c107c1a3c63067d72067a
Guilherme Soares <g.l.soares@outlook.com>
2023-07-27 11:21:22 -0300

    aula de gulp

--- Modulo_16/gulpfile.js
+++ Modulo_16/gulpfile.js
@@ -1,16 +1,35 @@
-const gulp = require('gulp')
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
+
+function compilaSass(){
+    return gulp.src('./source/styles/main.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle:"compressed"
+        }))
+        .pipe(sourcemaps.write("./maps"))
+        .pipe(gulp.dest('./build'))
+}
+
 
 function numeroUm(callback){
     setTimeout(function(){
         console.log("Olá!")
         callback()
-    }, 2000)
+    }, 15000)
 }
 
 function numeroDois(callback){
-    console.log("Tudo bem?")
-    callback()
+    setTimeout(function(){
+        console.log("Tudo bem?")
+        callback()
+    },15000)
 }
 
 exports.default = gulp.series(numeroUm, numeroDois)
 exports.n = gulp.parallel(numeroUm, numeroDois)
+exports.s = compilaSass
+exports.watch = function(){
+    gulp.watch("./source/styles/*.scss", {ignoreInitial:false}, gulp.series(compilaSass))
+}
